1c7aa226e4578905a42f24fe4c021313aab74eb8
DiegoFischerDev <diegofischer.dev@gmail.com>
2023-07-26 16:32:05 +0100

    @feat created dashboard mock screen and clientes screen

--- src/app/application/page.js
+++ src/app/application/page.js
@@ -1,59 +1,76 @@
+'use client'
+
 import Image from "next/image"
 import logo from "../../assets/logo-ecotree.png"
 import userAdmin from "../../assets/user-admin.png"
 import LogoutButton from "@/components/Logout-Button"
 import { FiUser, FiLock } from "react-icons/fi";
 import MenuItem from "@/components/MenuItem";
+import { useState } from "react"
+import Dashboard from "@/components/Dashboard"
+import NovoOrçamento from "@/components/NovoOrçamento"
+import Clientes from "@/components/Clientes"
 
 export default function Application() {
+
+  const [showSection, setShowSection] = useState('Dashboard');
+
   return (
     <section className="flex text-2xl">
-      <div className="bg-slate-500 w-[200px]">
-        <div className="sticky left-0 top-0 flex flex-col items-center h-[100vh] justify-between">
-          <div className="flex items-center flex-col justify-center">
 
-            <div className="flex items-center flex-col w-[100%] justify-center mt-10">
-              <Image
-                src={userAdmin}
-                alt='Engineering logo'
-                width={500}
-                height={500}
-                className='w-12 rounded-[100%] border-2'
-                priority={true}
-              />
+      <div className="min-w-[200px]"></div>
 
-              <h2 className="text-white text-sm">Administrador</h2>
-              <h2 className="text-white text-sm">admin@ecotree.pt</h2>
-              <LogoutButton />
-            </div>
+      <div className="fixed left-0 top-0 bg-slate-500 flex flex-col items-center w-[200px] h-[100vh] justify-between">
+        <div className="flex items-center flex-col justify-center">
 
-            <ul className="mt-12">
-              <MenuItem title="Dashboard" />
-              <MenuItem title="Novo Orçamento" />
-              <MenuItem title="Clientes" />
-              <MenuItem title="Fornecedores" />
-              <MenuItem title="Menu 5" />
-              <MenuItem title="Menu 6" />
-            </ul>
-          </div>
-
-          <div className="text-xs text-gray-400 p-5 relative flex justify-center flex-col items-center">
+          <div className="flex items-center flex-col w-[100%] justify-center mt-10">
             <Image
-              src={logo}
+              src={userAdmin}
               alt='Engineering logo'
               width={500}
               height={500}
-              className='w-[140px]'
+              className='w-12 rounded-[100%] border-2'
               priority={true}
             />
-            <h2 className="absolute top-28 left-[45px] bg-slate-500 text-lg text-white">Ecotree.pt</h2>
-            <span>Versão 1.0</span><span>Ecotree &copy; 2023</span>
+
+            <h2 className="text-white text-sm">Administrador</h2>
+            <h2 className="text-white text-sm">admin@ecotree.pt</h2>
+            <LogoutButton />
           </div>
+
+          <ul className="mt-12">
+            <MenuItem title="Dashboard" showSection={showSection} setShowSection={setShowSection} />
+            <MenuItem title="Novo Orçamento" showSection={showSection} setShowSection={setShowSection} />
+            <MenuItem title="Clientes" showSection={showSection} setShowSection={setShowSection} />
+            <MenuItem title="Itens" showSection={showSection} setShowSection={setShowSection} />
+            <MenuItem title="Mão de Obra" showSection={showSection} setShowSection={setShowSection} />
+            <MenuItem title="Menu 6" showSection={showSection} setShowSection={setShowSection} />
+            <MenuItem title="Menu 7" showSection={showSection} setShowSection={setShowSection} />
+          </ul>
+        </div>
+
+        <div className="text-xs text-gray-400 p-5 relative flex justify-center flex-col items-center">
+          <Image
+            src={logo}
+            alt='Engineering logo'
+            width={500}
+            height={500}
+            className='w-[140px]'
+            priority={true}
+          />
+          <h2 className="absolute top-28 left-[58px] bg-slate-500 text-lg text-white">Ecotree</h2>
+          <span>Versão 1.0</span><span>Ecotree &copy; 2023</span>
         </div>
       </div>
 
-      <div className=" w-full p-10">
-        <h2>Dashboard</h2>
+      <div className="">
+        {showSection === "Dashboard" && <Dashboard />}
+        {showSection === "Novo Orçamento" && <NovoOrçamento />}
+        {showSection === "Clientes" && <Clientes />}
+        {showSection === "Itens" && "Itens"}
+        {showSection === "Mão de Obra" && "Mão de Obra"}
+        {showSection === "Menu 6" && "Menu 6"}
+        {showSection === "Menu 7" && "Menu 7"}
       </div>
 
     </section>
---
+++ src/components/NovoOrçamento.js
@@ -0,0 +1,115 @@
+'use client'
+
+import { useState } from "react";
+import { FiZoomIn, FiTrash2, FiTruck, FiPrinter } from "react-icons/fi";
+
+export default function NovoOrçamento() {
+
+  const orçamentos = [
+    {
+      id: 1,
+      cliente: "TRHK Engenharia",
+      valor: 800
+    },
+    {
+      id: 2,
+      cliente: "Compesa S.A.",
+      valor: 300
+    },
+    {
+      id: 3,
+      cliente: "Rodrigues Oliveira",
+      valor: 1200
+    },
+    {
+      id: 4,
+      cliente: "ANJE",
+      valor: 3500
+    },
+  ]
+
+  const [itemModificado, setItemModificado] = useState(0);
+
+  return (
+    <div className='p-10'>
+      <h2 className='text-gray-500 border-b-4 font-semibold border-gray-400'>Novo Orçamento</h2>
+
+      <div className='rounded border mt-10'>
+
+        <div className='bg-gray-200 p-2 px-4'>
+          <h3 className='text-gray-500'>Listagem dos Itens</h3>
+        </div>
+
+        <div className='flex'>
+          <div className='mx-5 ml-4 py-2 text-xl'>
+            <h4 className="h-10"></h4>
+            {orçamentos.map((orçamento) =>
+              <div key={orçamento.id} className="flex w-32 justify-between py-4 h-10 items-center mt-3">
+                <a className="cursor-pointer"><FiZoomIn color="purple" /></a>
+                <a className="cursor-pointer"><FiPrinter color="blue" /></a>
+                <a className="cursor-pointer"><FiTruck color="green" /></a>
+                <a className="cursor-pointer"><FiTrash2 color="red" /></a>
+              </div>)}
+          </div>
+
+          <div className='mx-5 p-2 text-xl'>
+            <h4 className="h-10 flex items-center justify-center">id</h4>
+            {orçamentos.map((orçamento) => <h4 key={orçamento.id} className="h-10 text-gray-400 mt-3 flex items-center justify-center">{orçamento.id}</h4>)}
+          </div>
+
+          <div className='mx-5 py-2 text-xl'>
+            <h4 className="h-10 flex items-center text-gray-600">Cliente</h4>
+            {orçamentos.map((orçamento) => <input type="text" onChange={(e) => { setItemModificado(orçamento.id) }} key={orçamento.id} placeholder={orçamento.cliente} className={`w-[180px] h-10 mt-3 flex items-center  ${orçamento.id === itemModificado ? "text-green-400 placeholder-green-400" : "text-gray-400 placeholder-gray-400"}`}></input>)}
+          </div>
+
+          <div className='mx-5 py-2 text-xl'>
+            <h4 className="h-10 flex items-center text-gray-600">Categoria</h4>
+            {orçamentos.map((orçamento) => <select onChange={() => { setItemModificado(orçamento.id) }} className={`w-[130px] h-10 mt-3 flex items-center  ${orçamento.id === itemModificado ? "text-green-400" : "text-gray-400"}`}>
+              <option value="volvo">Reforma</option>
+              <option value="saab">Pintura</option>
+              <option value="opel">Obra</option>
+              <option value="audi">Outras</option>
+            </select>)}
+          </div>
+
+          <div className='mx-5 py-2 text-xl'>
+            <h4 className="h-10 flex items-center text-gray-600">Forma de Pgmt</h4>
+            {orçamentos.map((orçamento) => <select onChange={() => { setItemModificado(orçamento.id) }} className={`w-[130px] h-10 mt-3 flex items-center  ${orçamento.id === itemModificado ? "text-green-400" : "text-gray-400"}`}>
+              <option value="volvo">Dinheiro</option>
+              <option value="saab">Multibanco</option>
+              <option value="opel">MB-Way</option>
+              <option value="audi">Outras</option>
+            </select>)}
+          </div>
+
+          <div className='mx-5 py-2 text-xl'>
+            <h4 className="h-10 flex items-center w-[160px] text-gray-600">Data da Emissão</h4>
+            {orçamentos.map((orçamento) => <input type="date" onChange={() => { setItemModificado(orçamento.id) }} key={orçamento.id} className={`w-[140px] h-10 mt-3 flex items-center  ${orçamento.id === itemModificado ? "text-green-400" : "text-gray-400"}`}></input>)}
+          </div>
+
+          <div className='mx-5 py-2 text-xl'>
+            <h4 className="h-10 flex items-center w-[150px] text-gray-600">Data do Pgmt</h4>
+            {orçamentos.map((orçamento) => <input type="date" onChange={() => { setItemModificado(orçamento.id) }} key={orçamento.id} className={`w-[140px] h-10 mt-3 flex items-center  ${orçamento.id === itemModificado ? "text-green-400" : "text-gray-400"}`}></input>)}
+          </div>
+
+          <div className='mx-5 py-2 text-xl'>
+            <h4 className="h-10 flex items-center text-gray-600">Valor €</h4>
+            {orçamentos.map((orçamento) =>
+              <div className={`flex items-center mt-3 ${orçamento.id === itemModificado ? "text-green-400 placeholder-green-400" : "text-gray-400 placeholder-gray-400"}`}>
+                <input type="number" onChange={() => { setItemModificado(orçamento.id) }} key={orçamento.id} placeholder={orçamento.valor} className={`w-[80px] h-10 flex items-center ${orçamento.id === itemModificado ? "text-green-400 placeholder-green-400" : "text-gray-400 placeholder-gray-400"}`}></input>
+                €
+              </div>
+            )}
+          </div>
+
+          <div className='mx-5 py-2 text-xl'>
+            <h4 className="h-10 flex items-center text-gray-600"></h4>
+            {orçamentos.map((orçamento) => <button onClick={() => { setItemModificado(0) }} key={orçamento.id} className={`w-[100px] ${orçamento.id === itemModificado ? "bg-green-400" : "bg-gray-400"} h-10 mt-3 flex items-center justify-center text-white rounded`}>Salvar</button>)}
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
